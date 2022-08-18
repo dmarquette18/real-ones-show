c822f26898b24f2115a7bdc3a751f50713700f6d
David Marquette <dmarquette.18@agileblue.com>
2022-08-18 01:08:37 -0400

    fdg

--- src/artistCard.js
+++ src/artistCard.js
@@ -1,5 +1,5 @@
 import HoverVideoPlayer from 'react-hover-video-player';
-import React, { useEffect } from "react";
+import React from "react";
 import {motion} from 'framer-motion';
 
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import LandingPage from './LandingPage.js';
 import reportWebVitals from './reportWebVitals';
-import TicketCard from './TicketCard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
